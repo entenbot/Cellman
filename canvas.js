08c2f810b6c96afc7d6f65da386dbeb81672ed80
entenbot <entengeschichten@protonmail.com>
2019-08-23 00:12:36 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,5 +1,7 @@
 
-//gets postion coordinate from mouse (wich row/collumn)
+//CELLMAN
+
+//gets postion coordinate from mouse (wich row/collumn was clicked)
 function coordinates(mousePosition){           
     if (mousePosition < cellSize){
         return 0;
@@ -11,7 +13,6 @@ function coordinates(mousePosition){
     return position-1;
 };
 
-
 //translates position coordinates from cordinates to canvas position
 function toField (pos){    
     pos = (pos+1)*cellSize;
@@ -21,8 +22,6 @@ function toField (pos){
         return pos - cellSize;
     };
 };
-
-
 
 //counting max rows and collumns
 function maxRows(cellSize){
@@ -47,27 +46,33 @@ function maxCollumns(cellSize){
      return maxGrid -=1;
 };
 
-
-//   clears canvas
+//clears canvas
 function cls(){
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
-   
+};
+
+///clears field
+function clears(){
+    gameBoard = [];
+    cls();
+    drawField();
 };
 
 //draws field
 function drawField(){ 
+    let maxC = maxCollumns(cellSize);
+    let maxR = maxRows(cellSize);
     let row = 0;
     let collumn = 0;
-    for (i= 0; i < maxCollumns(cellSize); i++){
+    for (i= 0; i < maxC; i++){
         row = 0;
-        for (b = 0; b< maxRows(cellSize); b++){
+        for (b = 0; b< maxR; b++){
             ctx.rect(row,collumn,cellSize,cellSize);
             ctx.stroke();
             row += cellSize;
         } 
         collumn += cellSize;
-}
-}
+}}
 
 function randomRgb(){
     let r = Math.floor(Math.random()*41);
@@ -83,144 +88,74 @@ function randomRgbReddish(){
     return 'rgba('+r+', '+g+', '+b+')';     
 };
 
-
-
-
-
-
 function drawBoard(){
     gameBoard.forEach(element => {
-        
-        
-        
-        
         ctx.fillStyle = randomRgbReddish();
         ctx.fillRect(toField(element.x),toField(element.y),cellSize,cellSize);
-    
     });
 };
 
+//checks if stone is on gameboard list
+function isUnique(object){
+    if (object.x == stone.x && object.y == stone.y){
+        return false;
+    } else { 
+        return true;
+    }
+};
 
-
-
-
-
-
-
-
-
-
+//animation loop
+function loop()
+{
+    drawBoard();
+    
+};
+setInterval(loop,200);
 
 //getting input from webpage and setting up the canvas element. 
 
 var cellSize = parseInt(document.getElementById('inputCellSize').value);
 var canvas = document.querySelector('canvas');
+ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight- (window.innerHeight*.20);
+canvas.width = cellSize*maxRows(cellSize);
+canvas.height = cellSize*maxCollumns(cellSize);
 
-ctx = canvas.getContext('2d');
+drawField();
 
-var animationSpeed = 10;
 var gameBoard = [];
 var stone = {};
-
-
-
 
 //adds clicked fields to map
 ctx.canvas.addEventListener('click', function(event) {
     var mouseX = event.clientX - ctx.canvas.offsetLeft;
     var mouseY = event.clientY - ctx.canvas.offsetTop;
-    
     document.querySelector('output').innerHTML = coordinates(mouseX) + '|' +coordinates(mouseY);
-    
-    ctx.fillStyle = randomRgb();
-    ctx.fillRect(toField(coordinates(mouseX)),toField(coordinates(mouseY)),cellSize,cellSize);
-
+    //ctx.fillStyle = randomRgb();
+    //ctx.fillRect(toField(coordinates(mouseX)),toField(coordinates(mouseY)),cellSize,cellSize);
     stone = {x: coordinates(mouseX),y: coordinates(mouseY),l: 1};
-    
     
     //need to check if stone object is allready clicked and in the gameboard list
     
     let wasDeleted = false;
-    console.clear();
-    
-    
     gameBoard.forEach(element => {
         if (element.x == stone.x && element.y == stone.y){
-            console.log('exists');
-            
-            
             ctx.fillStyle='rgb(252, 224, 190)'
             ctx.fillRect(toField(element.x),toField(element.y),cellSize,cellSize);
             ctx.rect(toField(element.x),toField(element.y),cellSize,cellSize);
             ctx.stroke();
-
-            
             wasDeleted = true;
         } 
     });
-
-    
-    function isUnique(object){
-        if (object.x == stone.x && object.y == stone.y){
-            return false;
-        } else { 
-            return true;
-        }
-    }
-    
-    const results = gameBoard.filter(isUnique);
-    gameBoard = results;
-
-    
+    gameBoard = gameBoard.filter(isUnique);
     if (wasDeleted == false){
-    gameBoard.push(stone);
-};
+        gameBoard.push(stone);
+    };
     gameBoard.forEach(element => {
-    console.log(element);
-    
+        console.log(element);
+    })
 })
     
-
-    
-    
-    
-    
-
-    
-    
-    
-})
-    
-
-
-
-
-
-
-
-
-
-
-canvas.width = cellSize*maxRows(cellSize);
-canvas.height = cellSize*maxCollumns(cellSize);
-
-
-
-drawField();
-
-
-
-function loop(){
-  drawBoard();
-}
-setInterval(loop,200);
-
-
-
-
-
-
 
